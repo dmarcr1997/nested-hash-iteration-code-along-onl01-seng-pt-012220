4689af7fa53597cc2c186b814226ba4672a8c38f
Damon R <dmarcr1997@gmail.com>
2020-01-27 16:46:58 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -21,9 +21,6 @@ def remove_strawberry(contacts)
         if attribute == :favorite_ice_cream_flavors
           data.delte_if {|ice_cream| ice_cream == "strawberry"}
         end
-            
-        end
       end
     end
   end
-end
